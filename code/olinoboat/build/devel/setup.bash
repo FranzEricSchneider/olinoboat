300551e78e202efb55ac93ac037251139bde7a07
David N Gaynor <dgaynor@ubuntu.(none)>
2013-03-03 22:49:24 -0800

    removed the build folder from version control, so we don't get weird build errors across our different computers

--- code/olinoboat/build/devel/setup.bash
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# generated from catkin/cmake/templates/setup.bash.in
-
-CATKIN_SHELL=bash
-. "/home/bradpowers/groovy_workspace/sandbox/olinoboat/code/olinoboat/build/devel/setup.sh"
--- code/olinoboat/build/devel/setup.zsh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env zsh
-# generated from catkin/cmake/templates/setup.zsh.in
-
-CATKIN_SHELL=zsh
-emulate sh # emulate POSIX
-. "/home/bradpowers/groovy_workspace/sandbox/olinoboat/code/olinoboat/build/devel/setup.sh"
-emulate zsh # back to zsh mode
