8f2d2919d6f075cd40e9e156bc468d85484c3c12
David N Gaynor <dgaynor@ubuntu.(none)>
2013-03-03 23:53:13 -0800

    cleaned the launch file a bit, add an env var so it doesn't get version control failed. Got the launchfile to launch the listener and talker with output to the console

--- code/olinoboat/nodes/listener.py
+++ code/olinoboat/nodes/listener.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python
-import roslib; roslib.load_manifest('beginner_tutorials')
+import roslib; roslib.load_manifest('olinoboat')
 import rospy
 from std_msgs.msg import String
 
-
 def callback(data):
-    rospy.loginfo(rospy.get_name() + ": I heard %s" % data.data)
-
+	rospy.loginfo(rospy.get_name() + ": I heard %s" % data.data)
 
 def listener():
     rospy.init_node('listener', anonymous=True)
--- code/olinoboat/nodes/talker.py
+++ code/olinoboat/nodes/talker.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
-import roslib; roslib.load_manifest('beginner_tutorials')
+import roslib; roslib.load_manifest('olinoboat')
 import rospy
 from std_msgs.msg import String
+
 
 
 def talker():
     pub = rospy.Publisher('chatter', String)
     rospy.init_node('talker')
+    
     while not rospy.is_shutdown():
         str = "hello world %s" % rospy.get_time()
         rospy.loginfo(str)
@@ -16,6 +18,7 @@ def talker():
 
 if __name__ == '__main__':
     try:
+        print("look at me, yeaaaa")
         talker()
     except rospy.ROSInterruptException:
         pass
