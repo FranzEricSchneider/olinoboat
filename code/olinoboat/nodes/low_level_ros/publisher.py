6c675e012f103d824a3f36dd35855107f06376b6
David N Gaynor <dgaynor@ubuntu.(none)>
2013-03-04 15:13:54 -0800

    did a bit more work modularizing the pub sub stuff, my python is real jank right now, with globals and stuff, I'm gonna change it to classes later

--- code/olinoboat/nodes/low_level_ros/publisher.py
+++ code/olinoboat/nodes/low_level_ros/publisher.py
@@ -2,26 +2,25 @@
 import roslib; roslib.load_manifest('olinoboat')
 import rospy
 from std_msgs.msg import String
+pub = 0
 
 
-
-def initialize(node_name,topic_to_publish_to,publish_loop):
+def initialize(node_name,topic_to_publish_to):
+    global pub 
     pub = rospy.Publisher(topic_to_publish_to, String)
     rospy.init_node(node_name)
-    r = rospy.Rate(1)
 
-    while not rospy.is_shutdown():
-        str = publish_loop()
-        rospy.loginfo("now publishing: "+str)
-        pub.publish(String(str))
-        r.sleep()
-
-def _test_publish_loop():
-    return "hey there worldy world"
+def publish(message):
+    global pub
+    if pub: 
+        rospy.loginfo(message)
+        pub.publish(message)
+    else:
+        print "you must initialize before publishing"
 
 if __name__ == '__main__':
     try:
         print("look at me, yeaaaa")
-        initialize("talker","chatter",_test_publish_loop)
+        initialize("talker","chatter")
     except rospy.ROSInterruptException:
         pass
