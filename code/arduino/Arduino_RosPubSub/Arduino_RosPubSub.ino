c787d9a88fcc14ffcb0eace7b85433a6ab014496
Trevor <trevor@trevor-Latitude-E6400.(none)>
2013-03-07 18:05:41 -0500

    Added water switch

--- code/arduino/Arduino_RosPubSub/Arduino_RosPubSub.ino
+++ code/arduino/Arduino_RosPubSub/Arduino_RosPubSub.ino
@@ -18,11 +18,14 @@
 ros::NodeHandle  nh;
     
 // Set variables to be used later format: datatype variablename
-unsigned long duration;
 int encoder_pin = 8;
 int servo1_pin = 9;
 int servo2_pin = 10;
+int water_pin = A1;
+unsigned long duration;
+int water_voltage = 0;
 std_msgs:: UInt16 pwm_msg;      // The encoder outputs a 16 bit unsigned integer for the amount of time the pulse is high
+std_msgs:: UInt16 water_msg;    // The water sensor outputs a value for its voltage between 0V and 5V depending on whether the circuit senses water or not.
 std_msgs:: UInt16 compass_msg;  // The compass outputs a 16 bit unsigned integer from 0 at North clockwise to 360 if you've calibrated.
 std_msgs:: UInt16 servo1_msg;
 std_msgs:: UInt16 servo2_msg;
@@ -51,6 +54,7 @@ Servo servo2;
 
 // define publishers: publishername("topic_name", &message);
   ros::Publisher pub_pwm("pwm_duration", &pwm_msg);
+  ros::Publisher pub_water("leak", &water_msg);
   ros::Publisher pub_compass("heading", &compass_msg);
 
 // This is the setup code that runs on startup and calls the functions defined above.
@@ -60,9 +64,11 @@ void setup(){
   nh.initNode();
   
   // Set up Arduino hardware pins
-  pinMode(encoder_pin, INPUT); //attach encoder to pin 8
-  servo1.attach(servo1_pin); //attach servo1 to pin 9
-  servo2.attach(servo2_pin); //attach servo2 to pin 10
+  pinMode(encoder_pin, INPUT); // set encoder_pin to input
+  pinMode(water_pin, INPUT); // set water_pin to input
+  pinMode(13, OUTPUT);
+  servo1.attach(servo1_pin); //attach servo1 to servo1_pin
+  servo2.attach(servo2_pin); //attach servo2 to servo2_pin
 
   // Set up Compass
   Wire.begin();
@@ -77,18 +83,29 @@ void setup(){
   // "Advertise" to the node the topics being published	
   nh.advertise(pub_pwm);
   nh.advertise(pub_compass);
+  nh.advertise(pub_water);
       
   // Subscribe to the node to the servo control topics
   nh.subscribe(sub1);
   nh.subscribe(sub2);
 }
 
+
 // This is the code that runs repeatedly until you shut it down.
 void loop(){
   nh.spinOnce();
 
-  // Collecting the encoder PWM signal
+  // Collecting the encoder PWM signal for relative wind direction
   pwm_msg.data = pulseIn(encoder_pin, HIGH);
+  
+  // Collecting water sensor voltage
+  water_voltage = analogRead(water_pin);
+  if (water_voltage > 1) {
+    digitalWrite(13, HIGH);
+  }
+  else {
+    digitalWrite(13, LOW);
+  }
 
   // Collecting compass input vector
   compass.read();
@@ -96,6 +113,7 @@ void loop(){
 
   // Publish each new message.
   pub_pwm.publish(&pwm_msg);
+  pub_water.publish(&water_msg);
   pub_compass.publish(&compass_msg);
   delay(1);
 }
--- code/olinoboat/nodes/data_subscriber.py
+++ code/olinoboat/nodes/data_subscriber.py
@@ -9,6 +9,7 @@ class datalistener():
     def __init__(self, offset = 0, node = 0):
         self.offset = offset
 	self.wind_angle = 0
+	self.leak = 0
 	self.compass_angle = 0
 	self.gps_pos = [0,0,0]
 
@@ -19,6 +20,7 @@ class datalistener():
 	#set up subscribers to ("topic", DataType, callback_function)
         rospy.Subscriber("pwm_offset", UInt16, self.offset_callback)
         rospy.Subscriber("pwm_duration", UInt16, self.pwm_callback)
+	rospy.Subscriber("leak", UInt16, self.water_callback)
         rospy.Subscriber("heading", UInt16, self.compass_callback)
         rospy.Subscriber("GPS_output", UInt16, self.gps_callback)
 
@@ -29,6 +31,11 @@ class datalistener():
     def pwm_callback(self, data):
         phigh = data.data
         self.wind_angle = (360*(phigh - self.offset)/1024.)%360
+
+    def water_callback(self, data):
+        if data.data > 1:
+            self.leak = 1
+	else self.leak = 0	#May need to comment this out, reset self.leak manually in the event of a leak.
 
     def compass_callback(self, data):
         self.compass_angle = data.data
